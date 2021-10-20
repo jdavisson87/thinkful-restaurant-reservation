87fe1d970ea901625c51cadf4cafd6356c5f5282
jdavisson87 <jdavisson87@gmail.com>
2021-10-19 20:43:12 -0500

    Removed styled components to comply with the requirements.  Will use vanilla css and bootstrap

--- front-end/src/Containers/layout/Layout.js
+++ front-end/src/Containers/layout/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Menu from '../../Components/Menu/Menu';
 import Routes from '../../Routes';
-import { SideBar } from './Layout.styles';
 
 /**
  * Defines the main layout of the application.
@@ -14,9 +13,9 @@ const Layout = () => {
   return (
     <div className="container-fluid">
       <div className="row h-100">
-        <SideBar className="col-md-2">
+        <div className="col-md-2">
           <Menu />
-        </SideBar>
+        </div>
         <div className="col">
           <Routes />
         </div>
--- front-end/src/Containers/layout/Layout.styles.jsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { color } from '../../utils/color-palette';
-
-export const SideBar = styled.div`
-  background: ${color.dark};
-`;
