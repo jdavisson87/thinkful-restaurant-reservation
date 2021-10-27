73b9a330f016823206a5a33d8e5775ca48bd2800
jdavisson87 <jdavisson87@gmail.com>
2021-10-27 11:56:38 -0500

    added styling element where shape of table changes based on how many seats

--- front-end/src/Components/Table/Table.js
+++ front-end/src/Components/Table/Table.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Table.css';
 
-const Table = () => {
+const Table = ({ table_name = 'table', size = 1, reserved = false }) => {
+  let status = reserved === false ? 'Free' : 'Occupied';
+  let btnColor = reserved === false ? 'btn-success' : 'btn-danger';
+  let tableShape = size <= 2 ? 'double' : size <= 4 ? 'quad' : 'great';
+
   return (
     <div className="card tableCard">
-      <h2>Table</h2>
+      <div className="card-header">{table_name}</div>
+      <div className="d-flex justify-content-center tableShape">
+        <div className={`${tableShape}`} />
+      </div>
+      <h5>Seats: {size}</h5>
+      <button className={`btn ${btnColor}`}>{status}</button>
     </div>
   );
 };
