2a866d90fff0c238dd79d9ebdb157451fe456024
jdavisson87 <jdavisson87@gmail.com>
2021-10-30 13:25:49 -0500

    reservation item that will show for each reservation

--- front-end/src/Components/ReservationItem/ReservationItem.js
+++ front-end/src/Components/ReservationItem/ReservationItem.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-const ReservationItem = () => {
-  return(
-    <div>
-      <p>ReservationItem</p>
+const ReservationItem = ({
+  first = 'jeff',
+  last = 'dav',
+  mobile = '123-456-7890',
+  size = '1',
+  reservationTime = '3:00PM',
+}) => {
+  return (
+    <div className="card">
+      <div className="card-header">
+        {first} {last}
+      </div>
+      <div className="card-body p-2 container">
+        <div className="row">
+          <span className="col-6">Mobile: {mobile}</span>
+          <span className="col-6 text-center"> Size: {size}</span>
+        </div>
+        <div className="row">
+          <span className="col-6">Reservation Time: {reservationTime}</span>
+          <div className="d-flex col-6 justify-content-center">
+            <button className="btn btn-primary">Seat</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ReservationItem
+export default ReservationItem;
