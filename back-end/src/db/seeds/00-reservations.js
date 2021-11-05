9fac7102a3a5b40cb3ccf10c7334cddfbbb45f49
jdavisson87 <jdavisson87@gmail.com>
2021-11-05 14:36:00 -0500

    created seed files to populate reservations and initial tables

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,11 @@
+const reservations = require('./00-reservations.json');
+
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
+  return knex('reservations')
+    .del()
+    .then(function () {
+      return knex
+        .raw('TRUNCATE TABLE reservations RESTART IDENTITY CASCADE')
+        .then(() => knex('reservations').insert(reservations));
+    });
 };
