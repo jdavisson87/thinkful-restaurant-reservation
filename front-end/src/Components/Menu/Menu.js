aee65189587664fa53f65a39c47eaf6798c17028
jdavisson87 <jdavisson87@gmail.com>
2021-12-08 15:45:26 -0600

    adjusted menu header and margins for button

--- front-end/src/Components/Menu/Menu.js
+++ front-end/src/Components/Menu/Menu.js
@@ -14,16 +14,16 @@ const Menu = () => {
 
   // if screen size decreases, need to toggle back to true
   return (
-    <nav className="navbar navbar-dark align-items-start p-0">
+    <nav className="navbar navbar-dark align-items-start p-0 m-2">
       <div className="container-fluid d-flex flex-column p-0">
         <Link
           className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
           to="/"
         >
-          <div className="sidebar-brand-text mx-0">
-            <span>Periodic Tables</span>
-            <br />
-            <span> Reservation System</span>
+          <div className="sidebar-brand-text text-center mx-0">
+            <h3 className="m-0 p-0 text-wrap">
+              Periodic Tables Reservation System
+            </h3>
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
@@ -55,7 +55,7 @@ const Menu = () => {
             </li>
           </ul>
         )}
-        <div className="text-center d-none d-md-inline">
+        <div className="text-center d-none d-md-inline my-3">
           <button
             className="btn rounded-circle border-0 plus-btn"
             id="sidebarToggle"
