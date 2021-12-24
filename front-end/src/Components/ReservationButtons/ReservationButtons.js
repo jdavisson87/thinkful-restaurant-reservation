c51245d9e16a182181ca79f5da07d395c2b1cb1a
jdavisson87 <jdavisson87@gmail.com>
2021-12-24 15:42:09 -0600

    created component that holds buttons for reservations

---
+++ front-end/src/Components/ReservationButtons/ReservationButtons.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const ReservationButtons = ({ onCancel, onSeat, id, time }) => {
+  const history = useHistory();
+  return (
+    <div className="row">
+      <span className="col-6">Reservation Time: {time}</span>
+      <div className="d-flex col-6 justify-content-end">
+        <button
+          className="btn btn-success m-1"
+          onClick={() => history.push(`/reservations/${id}/edit`)}
+        >
+          <span className="oi oi-pencil mr-2 p-1">&nbsp;Edit</span>
+        </button>
+        <button className="btn btn-info m-1" onClick={onSeat}>
+          <span className="oi oi-thumb-up mr-2 p-1">&nbsp;Seat</span>
+        </button>
+        <button className="btn btn-danger m-1" onClick={onCancel}>
+          <span className="oi oi-ban mr-2 p-1">&nbsp; Cancel</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ReservationButtons;
