b51e7530f39c0caa3db1484cc7bec9633ee1fc74
jdavisson87 <jdavisson87@gmail.com>
2021-12-30 16:12:31 -0600

    reformatted reservation buttons

--- front-end/src/Components/ReservationButtons/ReservationButtons.js
+++ front-end/src/Components/ReservationButtons/ReservationButtons.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
-const ReservationButtons = ({ onCancel, id }) => {
-  const history = useHistory();
-  return (
-    <div className="d-flex col-6 justify-content-end">
-      <button
-        className="btn btn-success m-1"
-        onClick={() => history.push(`/reservations/${id}/edit`)}
-      >
-        <span className="oi oi-pencil mr-2 p-1">&nbsp;Edit</span>
-      </button>
-      <a className="btn btn-info m-1" href={`/reservations/${id}/seat`}>
-        <span className="oi oi-thumb-up mr-2 p-1">&nbsp;Seat</span>
-      </a>
-      <button className="btn btn-danger m-1" onClick={onCancel}>
-        <span className="oi oi-ban mr-2 p-1">&nbsp; Cancel</span>
-      </button>
-    </div>
-  );
-};
+const ReservationButtons = ({ onCancel, id }) => (
+  <div className="d-flex col-6 justify-content-end">
+    <a className="btn btn-success m-1" href={`/reservations/${id}/edit`}>
+      <span className="oi oi-pencil mr-2 p-1">&nbsp;Edit</span>
+    </a>
+    <a className="btn btn-info m-1" href={`/reservations/${id}/seat`}>
+      <span className="oi oi-thumb-up mr-2 p-1">&nbsp;Seat</span>
+    </a>
+    <button className="btn btn-danger m-1" onClick={onCancel}>
+      <span className="oi oi-ban mr-2 p-1">&nbsp; Cancel</span>
+    </button>
+  </div>
+);
 
 export default ReservationButtons;
