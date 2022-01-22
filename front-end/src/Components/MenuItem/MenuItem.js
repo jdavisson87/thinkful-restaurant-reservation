b3a1d646863c122bdf18d9119c1dd91f5ef9e1dc
jdavisson87 <jdavisson87@gmail.com>
2022-01-21 20:16:27 -0600

    removed h4 tag

--- front-end/src/Components/MenuItem/MenuItem.js
+++ front-end/src/Components/MenuItem/MenuItem.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const MenuItem = ({ address, design, name }) => {
   return (
-    <li className="nav-item h4 my-0 p-1" key={name}>
+    <li className="nav-item my-0 p-1" key={name}>
       <Link className="nav-link" to={address}>
         <span className={`oi oi-${design}`} />
         &nbsp;{name}
